acc05e4ea607c91061380bcbf960cbd604dfedf0
hyo <3794hyo@gmail.com>
2022-05-04 23:39:03 +0900

    Create resume.spec.ts

--- tests/resume.spec.ts
+++ tests/resume.spec.ts
@@ -16,12 +16,8 @@ test.describe('이력서', () => {
   test('경력 컴포넌트를 삭제하고 추가할 수 있다', async ({ page }) => {
     const loc = page.locator('[placeholder="회사명"]')
     await expect(loc).toBeVisible();
-
-    // 삭제
     await page.locator('text=X').first().click();
     await expect(loc).not.toBeVisible();
-    
-    // 추가
     await page.locator('text=+').first().click();
     await expect(loc).toBeVisible();
   })
@@ -33,6 +29,7 @@ test.describe('이력서', () => {
     await page.locator('textarea[name="job\\.0\\.description"]').fill('여러업무를 진행함.');
   });
 
+  
   test('경력 컴포넌트를 추가해서 작성할 수 있다', async ({ page }) => {
     await page.locator('text=+').first().click();
 
@@ -42,10 +39,36 @@ test.describe('이력서', () => {
     await page.locator('textarea[name="job\\.1\\.description"]').fill('여러업무2.');
   });
 
+  test('학력을 작성할 수 있다', async ({ page }) => {
+    await page.locator('[placeholder="학교명"]').fill('좋은학교');
+    await page.locator('[placeholder="전공 및 학위"]').fill('컴퓨터공학');
+    await page.locator('input[name="schoolPeriod"]').fill('2000.02 ~ 2004.02');
+    await page.locator('textarea[name="schoolContent"]').fill('여러가지 공부함.');
+  });
+
+  test('개인 프로젝트 컴포넌트를 삭제하고 추가할 수 있다', async ({ page }) => {
+    const loc = page.locator('[placeholder="자격증"]')
+    await expect(loc).toBeVisible();
+    await page.locator('text=X').nth(1).click();
+    await expect(loc).not.toBeVisible();
+    await page.locator('text=+').nth(2).click();
+    await expect(loc).toBeVisible();
+  });
+
+  test('개인 프로젝트를 작성할 수 있다', async ({ page }) => {
+    await page.locator('[placeholder="자격증"]').fill('좋은자격증');
+    await page.locator('input[name="certificate\\.0\\.period"]').fill('2000.02 ~ 2004.02');
+  });
+
+  test('외국어를 작성할 수 있다', async ({ page }) => {
+    await page.locator('[placeholder="외국어"]').fill('간단한 영어 회화');
+  });
+
   test('PDF 다운로드', async ({ page }) => {
     const [download] = await Promise.all([
       page.waitForEvent('download'),
       page.locator('text=PDF 다운로드').click()
     ]);
   })
+  
 });
