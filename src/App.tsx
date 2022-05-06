248970591e018377d01ff70db8e7d3c727348bae
hyo <3794hyo@gmail.com>
2022-05-06 17:45:30 +0900

    Change formik to react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,135 +1,137 @@
 import usePdfMake from './service/usePdfMake';
-import { Field, FieldArray, Form, Formik } from 'formik';
-import { PersistFormikValues } from 'formik-persist-values';
 import typesetting from './service/typesetting';
 import './App.css';
 import { Values } from './model/interface';
+import { FormProvider, SubmitHandler, useFieldArray, useForm, useFormContext } from "react-hook-form";
+
+function Jobs(){
+  const { register } = useFormContext();
+  const { fields, append, remove } = useFieldArray({ name: 'job' });
+
+  return (
+    <>
+      {fields.map((field: any, index: number) => (
+        <div key={field.id}>
+          <div style={{ float: 'right' }}>
+            <button
+              type="button"
+              onClick={() => remove(index)}
+            >
+              X
+            </button>
+          </div>
+          <label>회사명</label>
+          <input placeholder="회사명" {...register(`job.${index}.company`)} />
+          <label>직무</label>
+          <input placeholder="직무" {...register(`job.${index}.role`)} />
+          <label>기간</label>
+          <input placeholder="YYYY.MM - YYYY.MM" {...register(`job.${index}.period`)} />
+          <label>상세 업무 및 성과</label>
+          <textarea placeholder="상세 업무 및 성과" {...register(`job.${index}.description`)} />
+        </div>
+      ))}
+      <div className="flex-center">
+        <button
+          type="button"
+          onClick={() => append(1)}
+        >
+          +
+        </button>
+      </div>
+    </>
+  )
+}
+
+function Certificates(){
+  const { register } = useFormContext();
+  const { fields, append, remove } = useFieldArray({ name: 'certificate' });
+
+  return (
+    <>
+      {fields.map((field: any, index: number) => (
+        <div key={field.id}>
+          <div style={{ float: 'right' }}>
+            <button
+              type="button"
+              onClick={() => remove(index)}
+            >
+              X
+            </button>
+          </div>
+          <label>자격증</label>
+          <input placeholder="자격증" {...register(`certificate.${index}.name`)} />
+
+          <label>기간</label>
+          <input placeholder="YYYY.MM - YYYY.MM" {...register(`certificate.${index}.period`)} />
+        </div>
+      ))}
+      <div className="flex-center">
+        <button
+          type="button"
+          onClick={() => append(1)}
+        >
+          +
+        </button>
+      </div>
+    </>
+  )
+}
+
 
 function App() {
   const pdf = usePdfMake();
 
-  const handleSubmit = (values: Values) => {
+  const methods = useForm<Values>({
+    defaultValues:{
+      job: [{ company: '', role: '', description: '', period: '' }],
+      certificate: [{ name: '', period: '' }],
+    },
+  });
+
+  const { register, handleSubmit, watch, formState: { errors } } = methods
+  const onSubmit: SubmitHandler<Values> = data => {
     pdf
-      .createPdf(typesetting(values))
-      .download();
-  };
+      .createPdf(typesetting(data))
+      .download();  
+  }
 
   return (
     <div className="App">
-      <Formik
-        initialValues={{
-          name: '',
-          email: '',
-          phone: '',
-          site: '',
-          description: '',
-          school: '',
-          schoolMajor: '',
-          schoolPeriod: '',
-          schoolContent: '',
-          job: [{ 
-            company: '',
-            role: '', 
-            description: '', 
-            period: '', 
-          }],
-          certificate: [{ 
-            name: '',
-            period: '', 
-          }],
-          language: '',
-        }}
-        onSubmit={handleSubmit}
-        render={({ values }) => (
-          <Form autoComplete="off">
-            <div className="flex-end">
-              <button type="submit">
-                PDF 다운로드
-              </button>
-            </div>
-            <h2>기본 정보</h2>
-            <Field type="text" name="name" label="이름" placeholder="이름" />
-            <Field type="text" name="email" label="이메일" placeholder="이메일" />
-            <Field type="text" name="phone" label="연락처" placeholder="연락처" />
-            <Field type="text" name="site" label="블로그/웹사이트" placeholder="블로그/웹사이트" />
-            <Field type="text" name="description" label="소개" placeholder="소개" as="textarea" />
-            <h2>경력</h2>
-            <FieldArray
-              name="job"
-              render={arrayHelpers => (
-                <div>
-                  {values.job.map((job, index) => (
-                    <div key={index}>
-                      <div style={{ float: 'right' }}>
-                        <button
-                          type="button"
-                          onClick={() => arrayHelpers.remove(index)}
-                        >
-                          X
-                        </button>
-                      </div>
-                      <Field type="text" name={`job.${index}.company`} label="회사명" placeholder="회사명" />
-                      <Field type="text" name={`job.${index}.role`} label="직무" placeholder="직무" />
-                      <Field type="text" name={`job.${index}.period`} label="기간" placeholder="YYYY.MM - YYYY.MM" />
-                      <Field type="text" name={`job.${index}.description`} label="상세 업무 및 성과" placeholder="상세 업무 및 성과" as="textarea" />
-                    </div>
-                  ))}
-                  <div className="flex-center">
-                    <button
-                      type="button"
-                      onClick={() => arrayHelpers.push({ company: '' })}
-                    >
-                      +
-                    </button>
-                  </div>
-                </div>
-              )}
-            />
-            <h2>학력</h2>
-            <Field type="text" name="school" label="학교명" placeholder="학교명" />
-            <Field type="text" name="schoolMajor" label="전공 및 학위" placeholder="전공 및 학위" />
-            <Field type="text" name="schoolPeriod" label="기간" placeholder="YYYY.MM - YYYY.MM" />
-            <Field type="text" name="schoolContent" label="연구내용" placeholder="연구내용" as="textarea" />
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-              <button>+</button>
-            </div>
-            {/* <h2>개인 프로젝트</h2> */}
-            <h2>수상/자격증</h2>
-            <FieldArray
-              name="certificate"
-              render={arrayHelpers => (
-                <div>
-                  {values.certificate.map((certificate, index) => (
-                    <div key={index}>
-                      <div style={{ float: 'right' }}>
-                        <button
-                          type="button"
-                          onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
-                        >
-                          X
-                        </button>
-                      </div>
-                      <Field type="text" name={`certificate.${index}.name`} label="자격증" placeholder="자격증" />
-                      <Field type="text" name={`certificate.${index}.period`} label="기간" placeholder="YYYY.MM - YYYY.MM" />
-                    </div>
-                  ))}
-                  <div className="flex-center">
-                    <button
-                      type="button"
-                      onClick={() => arrayHelpers.push({ name: '' })}
-                    >
-                      +
-                    </button>
-                  </div>
-                </div>
-              )}
-            />
-            <h2>기타</h2>
-            <Field type="text" name="language" label="기타" placeholder="기타" />
-            <PersistFormikValues name="resume-form" />
-          </Form>
-        )}
-      />
+      <FormProvider {...methods} >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex-end">
+            <button type="submit">
+              PDF 다운로드
+            </button>
+          </div>
+
+          <h2>기본 정보</h2>
+          <input placeholder="이름" {...register("name")} />
+          <input placeholder="이메일" {...register("email")} />
+          <input placeholder="연락처" {...register("phone")} />
+          <input placeholder="블로그/웹사이트" {...register("site")} />
+          <textarea placeholder="소개" {...register("description")} />
+          
+          <h2>경력</h2>
+          <Jobs />
+            
+          <h2>학력</h2>
+          <input placeholder="학교명" {...register("school")} />
+          <input placeholder="전공 및 학위" {...register("schoolMajor")} />
+          <input aria-label='기간' placeholder="YYYY.MM - YYYY.MM" {...register("schoolPeriod")} />
+          <textarea placeholder="연구내용" {...register("schoolContent")} />
+
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <button>+</button>
+          </div>
+
+          <h2>수상/자격증</h2>
+          <Certificates />
+          
+          <h2>기타</h2>
+          <input placeholder="기타" {...register("language")} />
+        </form>
+      </FormProvider>
     </div >
   );
 }
