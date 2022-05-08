96b15eb1cadc7f9f78d631faf07d7495fbbd02d7
hyo <3794hyo@gmail.com>
2022-05-08 20:58:16 +0900

    Add server proxy to client vite config

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -2,9 +2,14 @@ import express, { Request, Response } from 'express'
 
 const app = express()
 
-app.get('/', async (req: Request, res: Response) => {
+app.get('/api', async (req: Request, res: Response) => {
   console.log('Hello world')
   res.send('Hello world')
+})
+
+app.put('/api/save', async (req: Request, res: Response) => {
+  console.log('save')
+  res.send('ok')
 })
 
 const port = 8080
