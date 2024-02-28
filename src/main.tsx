cb05d44f0c0004ccc39d6834034ff27e62da2a98
hyo <3794hyo@gmail.com>
2024-02-29 02:07:59 +0900

    Fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './application/App'
+import App from './application/app'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
