5bfe36d3d9fea63a0e90346afd823224088f51d2
hyo <3794hyo@gmail.com>
2024-02-29 00:57:11 +0900

    Update basic

--- src/application/Basics.tsx
+++ src/application/Basics.tsx
@@ -1,26 +1,29 @@
 import { basicProfilesDefaultValue } from '../model/defaultValues'
 import { useFieldArray, useFormContext } from 'react-hook-form'
-import useFieldArrayUtils from '../service/useFieldArrayUtils'
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 function Basics() {
   const { register } = useFormContext()
 
   return (
     <>
-      <h2>Basics</h2>
-      <input placeholder="Name" {...register('basics.name')} />
-      <input placeholder="Label" {...register('basics.label')} />
-      <input placeholder="Image" {...register('basics.image')} />
-      <input placeholder="Email" {...register('basics.email')} />
-      <input placeholder="Phone" {...register('basics.phone')} />
-      <input placeholder="URL" {...register('basics.url')} />
-      <input placeholder="Summary" {...register('basics.summary')} />
-      <input placeholder="Location.Address" {...register('basics.location.address')} />
-      <input placeholder="Location.PostalCode" {...register('basics.location.postalCode')} />
-      <input placeholder="Location.City" {...register('basics.location.city')} />
-      <input placeholder="Location.CountryCode" {...register('basics.location.countryCode')} />
-      <input placeholder="Location.Region" {...register('basics.location.region')} />
+      <h2 className="text-2xl">Basics</h2>
+
+      <div className="grid gap-2 mb-2">
+        <Input placeholder="Name" {...register('basics.name')} />
+        <Input placeholder="Label" {...register('basics.label')} />
+        <Input placeholder="Image" {...register('basics.image')} />
+        <Input placeholder="Email" {...register('basics.email')} />
+        <Input placeholder="Phone" {...register('basics.phone')} />
+        <Input placeholder="URL" {...register('basics.url')} />
+        <Input placeholder="Summary" {...register('basics.summary')} />
+        <Input placeholder="Location.Address" {...register('basics.location.address')} />
+        <Input placeholder="Location.PostalCode" {...register('basics.location.postalCode')} />
+        <Input placeholder="Location.City" {...register('basics.location.city')} />
+        <Input placeholder="Location.CountryCode" {...register('basics.location.countryCode')} />
+        <Input placeholder="Location.Region" {...register('basics.location.region')} />
+      </div>
       <Profiles />
     </>
   )
@@ -28,23 +31,27 @@ function Basics() {
 
 function Profiles() {
   const { register } = useFormContext()
-  // const { fields, Remove, Append } = useFieldArrayUtils({ name: 'basics.profiles' })
   const { fields, remove, append } = useFieldArray({ name: 'basics.profiles' })
 
 
   return (
     <>
       {fields.map((field: any, index: number) => (
-        <div key={field.id}>
-          <input placeholder="network" {...register(`basics.profiles.${index}.network`)} />
-          <input placeholder="username" {...register(`basics.profiles.${index}.username`)} />
-          <input placeholder="url" {...register(`basics.profiles.${index}.url`)} />
+        <div key={field.id} className="grid gap-2">
+          <Input placeholder="network" {...register(`basics.profiles.${index}.network`)} />
+          <Input placeholder="username" {...register(`basics.profiles.${index}.username`)} />
+          <Input placeholder="url" {...register(`basics.profiles.${index}.url`)} />
           {/* <Remove index={index} aria-label="remove basic profiles" /> */}
-          <Button type="button" onClick={() => remove()}>Remove</Button>
+          <Button type="button" onClick={() => remove(index)}>Remove</Button>
         </div>
       ))}
-      <Button type="button" onClick={() => append(undefined)}>ADD</Button>
-      {/* <Append defaultValue={basicProfilesDefaultValue} aria-label="append basic profiles" /> */}
+      <Button
+        type="button"
+        onClick={() => append(basicProfilesDefaultValue)}
+        aria-label="append basic profiles"
+      >
+        ADD
+      </Button>
     </>
   )
 }
